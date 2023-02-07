467def300e285b7771a8345fe07fccc5226ddde9
pekkenpah <akkapehonen@gmail.com>
2023-02-07 17:15:30 +0200

    ekat tehtävät

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122",
+      "id": 4
+    },
+ ]
+
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+
+  app.get('/info', (req, res) => {
+    date = new Date();
+    let response = "<div>Phonebook has info for " + persons.length + " people</div><div>" + date.toString() +  "</div>";
+    res.send(response)
+  })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => {      
+      return person.id === id
+    })
+    if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+  })
+
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
