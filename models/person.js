a52fd4a267b2157abfe6bdc0ce534e5783685b68
pekkenpah <akkapehonen@gmail.com>
2023-02-22 10:15:30 +0200

    3.21.2

--- models/person.js
+++ models/person.js
@@ -4,10 +4,12 @@ mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
+// yhdistetään kantaan
 console.log('connecting to', url)
+
 mongoose.connect(url)
   .then(result => {
-    console.log('connected to MongoDB')
+    console.log('connected to MongoDB.')
   })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
